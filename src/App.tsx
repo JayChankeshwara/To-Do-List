cc1616c13fae0a74d07548b254f15c7a9e32759c
Jay Chankeshwara <Jay.chankeshwara@gmail.com>
2026-08-09 01:08:54 +0530

    Initial React project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+
+  return (
+  <div>
+    <h1>Jay's Ultimate Todo App 🚀</h1>
+    <p>My first React application</p>
+
+    <button>Add Task</button>
+  </div>
+)
+}
+
+export default App
